6ba7d73ba9915614eef1b16a96d07b7884f2c7e1
bharatigore <bhartigore24@gmail.com>
2024-07-03 14:23:32 +0530

    add py files

---
+++ Python_Fundamentals/Client.py
@@ -0,0 +1,17 @@
+import Marvellous
+
+def main():
+    print("Enter first number : ")
+    A = int(input())
+    
+    print("Enter second number : ")
+    B = int(input())
+    
+    Ans = Marvellous.Addition(A,B)
+    print("Addition is : ",Ans)
+
+    Ans = Marvellous.Multiplication(A,B)
+    print("Multiplication is : ",Ans)
+
+
+main()
---
+++ Python_Fundamentals/createfileX.py
@@ -0,0 +1,15 @@
+import os
+
+def main():
+    print("Enter the name of file that you want to create : ")
+    Fname = input()
+
+    if os.path.exists(Fname):
+        print("Unable to create file as file is already existing")
+
+    else:
+        open(Fname, "x")
+        print("File gets succesfully created")
+
+if __name__ == "__main__":
+    main()
