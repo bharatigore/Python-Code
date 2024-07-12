208a6f479f9dba32aacf775f43eedf59b4284293
bharatigore <bhartigore24@gmail.com>
2024-07-12 11:23:07 +0530

    added files

---
+++ Automation_Scripts/ProcessAutomation3.py
@@ -0,0 +1,21 @@
+import psutil
+
+def CreateLog(FileName = "output.log"):
+    fd = open(FileName, "w")
+    separator = "-"*70
+
+    fd.write(separator + "\n")
+    fd.write(" Process Log"+ "\n")
+    fd.write(separator + "\n")
+    
+    fd.write("CONTENTS OF LOG FILE" + "\n")
+
+    fd.write(separator + "\n")
+
+    fd.close()
+
+def main():
+    CreateLog()
+
+if __name__ == "__main__":
+    main()
